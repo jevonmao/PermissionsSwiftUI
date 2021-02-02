f30b48d476190de5c31bbed6e1080678374a6e47
jevonmao <woodburyjevonmao@gmail.com>
2021-02-02 11:25:53 -0800

    Finish implementing custom text for all permissions

--- Sources/PermissionsSwiftUI/Modifiers/Customize/PermissionCustomModifiers.swift
+++ Sources/PermissionsSwiftUI/Modifiers/Customize/PermissionCustomModifiers.swift
@@ -12,7 +12,6 @@ struct PlaceholderModifier: ViewModifier {
         content
     }
 }
-
 // MARK: Camera
 
 public extension View {
@@ -30,7 +29,7 @@ public extension View {
                      Text("Ask user for permissions")
                  })
                  .JMPermissions(showModal: $showModal, for: [.locationAlways, .photo, .microphone])
-                 .permissionCamera(title: "Camcorder", description: "Allow to use the camera to film")
+                 .customizeCameraPermissionWith(title: "Camcorder", description: "Allow to use the camera to film")
              }
              
          }
@@ -40,7 +39,7 @@ public extension View {
         - description: The description text
      */
     
-    func permissionCamera(title: String, description: String) -> some View {
+    func customizeCameraPermissionWith(title: String, description: String) -> some View {
         PermissionModel.PermissionModelStore.cameraPermission.title = title
         PermissionModel.PermissionModelStore.cameraPermission.description = description
         return self.modifier(PlaceholderModifier())
@@ -53,7 +52,7 @@ public extension View {
         - title: The title text
      */
     
-    func permissionCamera(title: String) -> some View {
+    func customizeCameraPermissionWith(title: String) -> some View {
         PermissionModel.PermissionModelStore.cameraPermission.title = title
         return self.modifier(PlaceholderModifier())
     }
@@ -65,7 +64,7 @@ public extension View {
         - description: The description text
      */
     
-    func permissionCamera(description: String) -> some View {
+    func customizeCameraPermissionWith(description: String) -> some View {
         PermissionModel.PermissionModelStore.cameraPermission.description = description
         return self.modifier(PlaceholderModifier())
     }
@@ -88,7 +87,7 @@ public extension View {
                      Text("Ask user for permissions")
                  })
                  .JMPermissions(showModal: $showModal, for: [.locationAlways, .photo, .microphone])
-                 .permissionLocation(title: "Camcorder", description: "Allow to use the camera to film")
+                 .customizeLocationPermissionWith(title: "Camcorder", description: "Allow to use the camera to film")
              }
              
          }
@@ -98,7 +97,7 @@ public extension View {
         - description: The description text
      */
     
-    func permissionLocation(title: String, description: String) -> some View {
+    func customizeLocationPermissionWith(title: String, description: String) -> some View {
         PermissionModel.PermissionModelStore.locationPermission.title = title
         PermissionModel.PermissionModelStore.locationPermission.description = description
         return self.modifier(PlaceholderModifier())
@@ -107,12 +106,11 @@ public extension View {
     /**
      Customizes only the title text for location permission component
 
-     ````
      - Parameters:
         - title: The title text
      */
     
-    func permissionLocation(title: String) -> some View {
+    func customizeLocationPermissionWith(title: String) -> some View {
         PermissionModel.PermissionModelStore.locationPermission.title = title
         return self.modifier(PlaceholderModifier())
     }
@@ -124,8 +122,234 @@ public extension View {
         - description: The description text
      */
     
-    func permissionLocation(description: String) -> some View {
+    func customizeLocationPermissionWith(description: String) -> some View {
         PermissionModel.PermissionModelStore.locationPermission.description = description
+        return self.modifier(PlaceholderModifier())
+    }
+}
+
+// MARK: Calendar
+
+public extension View {
+    /**
+     Customizes the title and description text for calendar permission component
+     
+     For example, use this modifier on your existing view and pass in your custom text to show
+     ````
+         struct ContentView: View {
+             @State var showModal = false
+             var body: some View {
+                 Button(action: {
+                     showModal=true
+                 }, label: {
+                     Text("Ask user for permissions")
+                 })
+                 .JMPermissions(showModal: $showModal, for: [.locationAlways, .photo, .microphone])
+                 .customizeCalendarPermissionWith(title: "System Calendar", description: "Allow to read and write the calendar")
+             }
+             
+         }
+     ````
+     - Parameters:
+        - title: The title text
+        - description: The description text
+     */
+    
+    func customizeCalendarPermissionWith(title: String, description: String) -> some View {
+        PermissionModel.PermissionModelStore.calendarPermisson.title = title
+        PermissionModel.PermissionModelStore.calendarPermisson.description = description
+        return self.modifier(PlaceholderModifier())
+    }
+
+    /**
+     Customizes only the title text for calendar permission component
+
+     - Parameters:
+        - title: The title text
+     */
+    
+    func customizeCalendarPermissionWith(title: String) -> some View {
+        PermissionModel.PermissionModelStore.calendarPermisson.title = title
+        return self.modifier(PlaceholderModifier())
+    }
+
+    /**
+     Customizes only the description text for calendar permission component
+     
+     - Parameters:
+        - description: The description text
+     */
+    
+    func customizeCalendarPermissionWith(description: String) -> some View {
+        PermissionModel.PermissionModelStore.calendarPermisson.description = description
+        return self.modifier(PlaceholderModifier())
+    }
+}
+
+// MARK: Bluetooth
+
+public extension View {
+    /**
+     Customizes the title and description text for Bluetooth permission component
+     
+     - Parameters:
+        - title: The title text
+        - description: The description text
+     */
+    
+    func customizeBluetoothPermissionWith(title: String, description: String) -> some View {
+        PermissionModel.PermissionModelStore.bluetoothPermission.title = title
+        PermissionModel.PermissionModelStore.bluetoothPermission.description = description
+        return self.modifier(PlaceholderModifier())
+    }
+
+    /**
+     Customizes only the title text for Bluetooth permission component
+
+     - Parameters:
+        - title: The title text
+     */
+    
+    func customizeBluetoothPermissionWith(title: String) -> some View {
+        PermissionModel.PermissionModelStore.bluetoothPermission.title = title
+        return self.modifier(PlaceholderModifier())
+    }
+
+    /**
+     Customizes only the description text for Bluetooth permission component
+     
+     - Parameters:
+        - description: The description text
+     */
+    
+    func customizeBluetoothPermissionWith(description: String) -> some View {
+        PermissionModel.PermissionModelStore.bluetoothPermission.description = description
+        return self.modifier(PlaceholderModifier())
+    }
+}
+
+// MARK: Microphone
+
+public extension View {
+    /**
+     Customizes the title and description text for microphone permission component
+     
+     - Parameters:
+        - title: The title text
+        - description: The description text
+     */
+    
+    func customizeMicPermissionWith(title: String, description: String) -> some View {
+        PermissionModel.PermissionModelStore.microphonePermisson.title = title
+        PermissionModel.PermissionModelStore.microphonePermisson.description = description
+        return self.modifier(PlaceholderModifier())
+    }
+
+    /**
+     Customizes only the title text for microphone permission component
+
+     - Parameters:
+        - title: The title text
+     */
+    
+    func customizeMicPermissionWith(title: String) -> some View {
+        PermissionModel.PermissionModelStore.microphonePermisson.title = title
+        return self.modifier(PlaceholderModifier())
+    }
+
+    /**
+     Customizes only the description text for microphone permission component
+     
+     - Parameters:
+        - description: The description text
+     */
+    
+    func customizeMicPermissionWith(description: String) -> some View {
+        PermissionModel.PermissionModelStore.microphonePermisson.description = description
+        return self.modifier(PlaceholderModifier())
+    }
+}
+
+// MARK: Notification
+
+public extension View {
+    /**
+     Customizes the title and description text for notification permission component
+     
+     - Parameters:
+        - title: The title text
+        - description: The description text
+     */
+    
+    func customizeNotificationPermissionWith(title: String, description: String) -> some View {
+        PermissionModel.PermissionModelStore.notificationPermission.title = title
+        PermissionModel.PermissionModelStore.notificationPermission.description = description
+        return self.modifier(PlaceholderModifier())
+    }
+
+    /**
+     Customizes only the title text for notification permission component
+
+     - Parameters:
+        - title: The title text
+     */
+    
+    func customizeNotificationPermissionWith(title: String) -> some View {
+        PermissionModel.PermissionModelStore.notificationPermission.title = title
+        return self.modifier(PlaceholderModifier())
+    }
+
+    /**
+     Customizes only the description text for notification permission component
+     
+     - Parameters:
+        - description: The description text
+     */
+    
+    func customizeNotificationPermissionWith(description: String) -> some View {
+        PermissionModel.PermissionModelStore.notificationPermission.description = description
+        return self.modifier(PlaceholderModifier())
+    }
+}
+
+// MARK: Photo
+
+public extension View {
+    /**
+     Customizes the title and description text for the photo permission component
+     
+     - Parameters:
+        - title: The title text
+        - description: The description text
+     */
+    
+    func customizePhotoPermissionWith(title: String, description: String) -> some View {
+        PermissionModel.PermissionModelStore.photoPermission.title = title
+        PermissionModel.PermissionModelStore.photoPermission.description = description
+        return self.modifier(PlaceholderModifier())
+    }
+
+    /**
+     Customizes only the title text for the photo permission component
+
+     - Parameters:
+        - title: The title text
+     */
+    
+    func customizePhotoPermissionWith(title: String) -> some View {
+        PermissionModel.PermissionModelStore.photoPermission.title = title
+        return self.modifier(PlaceholderModifier())
+    }
+
+    /**
+     Customizes only the description text for the photo permission component
+     
+     - Parameters:
+        - description: The description text
+     */
+    
+    func customizePhotoPermissionWith(description: String) -> some View {
+        PermissionModel.PermissionModelStore.photoPermission.description = description
         return self.modifier(PlaceholderModifier())
     }
 }
---
+++ Sources/PermissionsSwiftUI/Modifiers/JMPermission API/MainModifiers.swift
@@ -0,0 +1,51 @@
+//
+//  SwiftUIView.swift
+//
+//
+//  Created by Jevon Mao on 1/30/21.
+//
+
+import SwiftUI
+
+// MARK: Public modifiers
+
+struct Permissions: ViewModifier {
+    @Binding var showModal: Bool
+
+    func body(content: Content) -> some View {
+        MainView(for: AnyView(content), show: $showModal)
+    }
+}
+
+public extension View {
+    /**
+     Displays a PermissionsSwiftUI modal view that displays and handles permissions.
+     
+     For example, use this modifier on your existing view and pass in a SwiftUI Binding boolean variable. This view will show a PermissionsSwiftUI modal with 3 different permissions.
+     ````
+         struct ContentView: View {
+             @State var showModal = false
+             var body: some View {
+                 Button(action: {
+                     showModal=true
+                 }, label: {
+                     Text("Ask user for permissions")
+                 })
+                 .JMPermissions(showModal: $showModal, for: [.locationAlways, .photo, .microphone])
+             }
+             
+         }
+     ````
+     - Parameters:
+        - showModal: A `Binding<Bool>` value to toggle show the JMPermission view
+        - for: An array of type `PermissionModel` to specify permissions to show
+     - Returns:
+        Returns a new view. Will show a modal that will overlay your existing view to show PermissionsSwiftUI permission handling components.
+     
+     */
+    
+    func JMPermissions(showModal: Binding<Bool>, for permissions: [PermissionModel]) -> some View {
+        PermissionModel.PermissionModelStore.permissions = permissions
+        return self.modifier(Permissions(showModal: showModal))
+    }
+}
