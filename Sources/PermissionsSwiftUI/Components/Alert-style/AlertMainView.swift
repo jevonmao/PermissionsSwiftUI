178217f9a832a958d1280ff41841a679f48366bc
Jevon Mao <woodburyjevonmao@gmail.com>
2021-02-11 19:20:48 -0800

    Added pop alert style permission display

--- Sources/PermissionsSwiftUI/Components/Alert-style/AlertMainView.swift
+++ Sources/PermissionsSwiftUI/Components/Alert-style/AlertMainView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct AlertMainView: View {
-    private var showModal: Binding<Bool>
+    private var showAlert: Binding<Bool>
+    var show:Bool{
+        get{
+            showAlert.wrappedValue
+        }
+        set{
+            showAlert.wrappedValue = newValue
+        }
+    }
     private var bodyView: AnyView
-    init(for bodyView: AnyView, show showModal: Binding<Bool>) {
+    init(for bodyView: AnyView, show showAlert: Binding<Bool>) {
         self.bodyView = bodyView
-        self.showModal = showModal
+        self.showAlert = showAlert
     }
     var body: some View {
         ZStack{
             bodyView
-            AlertView()
+            if show{
+                Group{
+                    Blur(style: .systemUltraThinMaterialDark)
+                        .edgesIgnoringSafeArea(.all)
+                        .transition(AnyTransition.opacity.animation(Animation.default.speed(1.8)))
+                    AlertView(showAlert:showAlert)
+                        .transition(AnyTransition.opacity.combined(with: .scale(scale: 0.92)).animation(Animation.default.speed(1.5)))
+                        .onAppear(perform: PermissionStore.shared.onAppear)
+                        .onDisappear(perform: PermissionStore.shared.onDisappear)
+                }
+               
+ 
+            }
         }
+
     }
 }
 
--- Sources/PermissionsSwiftUI/Components/Alert-style/AlertView.swift
+++ Sources/PermissionsSwiftUI/Components/Alert-style/AlertView.swift
@@ -8,16 +8,32 @@
 import SwiftUI
 
 struct AlertView: View {
+    @Binding var showAlert:Bool
+    var mainText:PermissionStore.MainTexts{PermissionStore.shared.mainTexts}
     var body: some View {
         ZStack{
+
             VStack{
-                HeaderText(exitButtonAction: {}, isAlert: true)
-                Spacer()
+                HeaderText(exitButtonAction: {showAlert = false}, isAlert: true)
+                PermissionSection(showModal: $showAlert, isAlert:true)
+                
+                if PermissionStore.shared.permissions.count < 2{
+                    Divider()
+                }
+                Text(mainText.bottomDescription)
+                    .font(.system(.caption, design: .rounded))
+                    .fontWeight(.regular)
+                    .foregroundColor(Color(.systemGray))
+                    .padding()
+                    .lineLimit(3)
+                    .frame(maxWidth:.infinity, alignment: .leading)
             }
             .padding(5)
+            .background(Color(.systemBackground).opacity(0.8))
+            .frame(width: screenSize.width-60)
+            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
         }
-        .background(Blur(style: .systemThinMaterialDark))
-        .frame(width: screenSize.width-60, height: screenSize.height/2)
-        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+
+        
     }
 }
