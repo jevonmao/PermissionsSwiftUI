300d406800cc2dcaa47b876936f2b89195a88647
Jevon Mao <woodburyjevonmao@gmail.com>
2021-02-11 20:37:23 -0800

    Make JMAlert more adaptive to screen sizes

--- Tests/PermissionsSwiftUITests/PermissionsSwiftUITests.swift
+++ Tests/PermissionsSwiftUITests/PermissionsSwiftUITests.swift
@@ -244,7 +244,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec congue metus.
         switch permission {
         default:
             let views:[AnyView] = AllowButtonStatus.allCases.map{
-                let view = PermissionSectionCell(permission: permission, allowButtonStatus: $0, showModal: .constant(true))
+                let view = PermissionSectionCell(permission: permission, allowButtonStatus: $0, showModal: .constant(true), isAlert: false)
                     .setPermissionComponent(for: permission, image: AnyView(Image(systemName: "gear")), title: "Testing \(title)", description: placeholderText)
                     .referenceFrameCell()
                 return AnyView(view)
