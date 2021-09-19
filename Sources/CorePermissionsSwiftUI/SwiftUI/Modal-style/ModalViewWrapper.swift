f9dc5a4e9dca98cfd3212de0f241614b59ac4b6d
Roberdan <roberdan@fightthestroke.org>
2021-09-19 11:55:25 +0200

    Update ModalViewWrapper.swift

--- Sources/CorePermissionsSwiftUI/SwiftUI/Modal-style/ModalViewWrapper.swift
+++ Sources/CorePermissionsSwiftUI/SwiftUI/Modal-style/ModalViewWrapper.swift
@@ -11,9 +11,9 @@ import Introspect
 @available(iOS 13.0, tvOS 13.0, *)
 @usableFromInline struct ModalViewWrapper<Body: View>: View, CustomizableView {
     //store contains static configurations and customizations
-    @ObservedObject var store: PermissionStore = PermissionStore.shared
+    @usableFromInline var store: PermissionStore = PermissionStore.shared
     //schemaStore contains dynamically computed properties, and internal methods/properties
-    @ObservedOnject var schemaStore: PermissionSchemaStore = PermissionSchemaStore.shared
+    @usableFromInline var schemaStore: PermissionSchemaStore = PermissionSchemaStore.shared
     //Keep track of whether modal as already been shown for 1st time
     @State var isModalNotShown = true
     @usableFromInline var showing: Binding<Bool>
