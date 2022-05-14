495165a61182bfe0a44a1b02cddb18c050451043
Jevon Mao <woodburyjevonmao@gmail.com>
2022-05-13 23:06:42 -0700

    Fix document coverage CI checking

--- .github/documentation_coverage.sh
+++ .github/documentation_coverage.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-percentage=$(jazzy --module CorePermissionsSwiftUI --swift-build-tool xcodebuild --build-tool-arguments -scheme,PermissionsSwiftUI,-sdk,iphoneos --author 'Jevon Mao' --author_url https://jingwen-mao.mit-license.org/ | tail -4 | head -n 1 | cut -d " " -f1
+percentage=$(jazzy --module CorePermissionsSwiftUI --swift-build-tool xcodebuild --build-tool-arguments -scheme,PermissionsSwiftUI,-sdk,iphoneos,-destination,id=6D8B3936-07C0-4F7D-8ADB-81B3E5BC4112,-verbose --author 'Jevon Mao' --author_url https://jingwen-mao.mit-license.org/ | tail -4 | head -n 1 | cut -d " " -f1
 )
+
+#if [ $? -eq 0 ]
+#then
+#    echo "/var/folders/24/"
 if test "${percentage%?}" == "100"
 then
 	echo "Documentation coverage 100%, test passed."
