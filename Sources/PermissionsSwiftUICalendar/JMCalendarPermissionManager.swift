6efe1e45fb89fa2d1562c75c841d0d07fa4b43c1
Jevon Mao <woodburyjevonmao@gmail.com>
2023-08-26 11:25:58 -0700

    Fix obsolete API mark and revise message

--- Sources/PermissionsSwiftUICalendar/JMCalendarPermissionManager.swift
+++ Sources/PermissionsSwiftUICalendar/JMCalendarPermissionManager.swift
@@ -12,7 +12,7 @@ import CorePermissionsSwiftUI
 
 @available(iOS 13.0, tvOS 13.0, *)
 public extension PermissionManager {
-    ///Permission that allows app to read & write to device calenda
+    ///Permission that allows app to read & write to device calendar
     @available(tvOS, unavailable)
     static let calendarFull = JMCalendarPermissionManager(requestedAccessLevel: .full)
 
@@ -21,7 +21,8 @@ public extension PermissionManager {
     static let calenderWrite = JMCalendarPermissionManager(requestedAccessLevel: .writeOnly)
 
     ///Permission that allows app to read & write to device calendar before iOS 17
-    @available(tvOS, unavailable, deprecated: 16.0, obsoleted: 17.0, message: "iOS 17.0 introduced breaking changes to calendar and reminder permissions. Use the new PermissionTypes that specifies level of permission.")
+    @available(tvOS, unavailable)
+    @available(iOS, deprecated, obsoleted: 17.0, message: "iOS 17.0 introduced breaking changes to EventKit APIs, use 'calendarFull' or 'calendarWrite' instead. Learn more at https://developer.apple.com/documentation/eventkit/accessing_the_event_store.")
     static let calendar = JMCalendarPermissionManager(requestedAccessLevel: .legacy)
 }
 
--- Sources/PermissionsSwiftUIReminder/JMRemindersPermissionManager.swift
+++ Sources/PermissionsSwiftUIReminder/JMRemindersPermissionManager.swift
@@ -13,7 +13,8 @@ import CorePermissionsSwiftUI
 @available(iOS 13.0, tvOS 13.0, *)
 public extension PermissionManager {
     ///Permission that allows app to read & write to device reminder before iOS 17
-    @available(tvOS, unavailable, deprecated: 16.0, obsoleted: 17.0, message: "iOS 17.0 introduced breaking changes to calendar and reminder permissions. Use the new PermissionTypes that specifies level of permission.")
+    @available(tvOS, unavailable)
+    @available(iOS, deprecated, obsoleted: 17.0, message: "iOS 17.0 introduced breaking changes to EventKit APIs. Learn more at https://developer.apple.com/documentation/eventkit/accessing_the_event_store.", renamed: "remindersFull")
     static let reminders = JMRemindersPermissionManager(requestedAccessLevel: .legacy)
 
     ///Permission that allows app to read & write to device reminder
@@ -21,46 +22,22 @@ public extension PermissionManager {
     static let remindersFull = JMRemindersPermissionManager(requestedAccessLevel: .full)
 }
 @available(iOS 13.0, tvOS 13.0, *)
-public final class JMRemindersPermissionManager: PermissionManager {
-    
-    public init(requestedAccessLevel: AccessLevel) {
-        self.requestedAccessLevel = requestedAccessLevel
-    }
-
-
-    public var requestedAccessLevel: AccessLevel
-
-    public enum AccessLevel {
-        case legacy, full
-    }
-
+public final class JMRemindersPermissionManager: EventPermissionManager {
     public override var permissionType: PermissionType {
         .reminders
     }
-    
-    public override var authorizationStatus: AuthorizationStatus {
-        switch EKEventStore.authorizationStatus(for: .reminder){
-        case .authorized:
-            return .authorized
-        case .notDetermined:
-            return .notDetermined
-        default:
-            return .denied
-        }
+
+    public override var entityType: EKEntityType {
+        .reminder
     }
 
     public override func requestPermission(completion: @escaping (Bool, Error?)->()) {
-        let eventStore = EKEventStore()
         if #available(iOS 17.0, *) {
-            //Request for permission using new API
+            eventStore.requestFullAccessToReminders(completion: completion)
         }
-
-        eventStore.requestAccess(to: EKEntityType.reminder, completion: {
-            (accessGranted: Bool, error: Error?) in
-            DispatchQueue.main.async {
-                completion(accessGranted, error)
-            }
-        })
+        else {
+            requestLegacyPermission(completion)
+        }
     }
 }
 #endif
